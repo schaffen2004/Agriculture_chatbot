112377c82e0f297d0d24f09fb655afc5fc064666
Schaffen <laphv494@gmail.com>
2024-10-01 22:52:35 +0700

    Upload agrichatbot

--- utils/authenticate.py
+++ utils/authenticate.py
@@ -1,3 +1,5 @@
+from models import account
+
 def check_login(username, password):
     # Giả sử kiểm tra thông tin đăng nhập (có thể thay thế bằng logic thực tế)
-    return username == "lap" and password == "123"
+    return account.check_account_credentials(username,password)
